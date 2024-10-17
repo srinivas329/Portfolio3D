f0180a8a025cfc962acca868a4215364c2cd6951
Srinivas <nagalapuramsrinu14@gmail.com>
2024-10-17 14:26:44 +0530

    added-footer-section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -78,7 +78,7 @@ const Works = () => {
       <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          className="mt-3 text-secondary text-[17px] leading-[30px]"
         >
           Following projects showcases my skills experience through real-world
           examlpes of my work. Each project is briefly described with links to
